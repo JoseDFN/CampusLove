a6f706456aaac35519b6c20318dd94537fc22164
JoseDFN <63378897+JoseDFN@users.noreply.github.com>
2025-05-13 20:31:54 -0500

    feat: :sparkles: Added base Ports
    Added the base Ports for each Entity

---
+++ Domain/Ports/IMatchRepository.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using SGCI_app.domain.Ports;
+
+namespace CampusLove.Domain.Ports
+{
+    public interface IMatchRepository : IGenericRepository<Match>
+    {
+        
+    }
+}
